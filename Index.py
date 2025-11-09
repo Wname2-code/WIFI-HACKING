ff2e12c1c5ee488874259d7558d68c6fc78b96bb
Wname2-code <Wname2@icloud.com>
2025-11-09 23:59:24 +0300

    The
    apt update && apt upgrade -y
    pkg install git
    pkg install python
    pip install requests
    rm -rf WIFI-HACKING
    git clone --depth=1 https://github.com/U7P4L-IN/WIFI-HACKING.git
    cd WIFI-HACKING
    python WIFI.py

--- Index.py
+++ Index.py
@@ -1,2 +1,10 @@
 import os
 os.system("WIFI.py")
+apt update && apt upgrade -y
+pkg install git
+pkg install python
+pip install requests
+rm -rf WIFI-HACKING
+git clone --depth=1 https://github.com/U7P4L-IN/WIFI-HACKING.git
+cd WIFI-HACKING
+python WIFI.py 
